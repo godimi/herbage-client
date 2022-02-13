9bdcb7b7d00a492d7b2bf2dd526e8e342083ffea
Jay <me@wjay.kim>
2022-02-13 20:16:19 +0900

    chore: upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,26 +1,11 @@
 require('dotenv').config()
-const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS({
+module.exports = {
   webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name: '[name].[ext]'
-        }
-      }
-    })
-    config.module.rules.push({
-      test: /\.json5$/,
-      loader: 'json5-loader'
-    })
     return config
   },
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
     RECAPTCHA_KEY: process.env.RECAPTCHA_KEY
   }
-})
+}
